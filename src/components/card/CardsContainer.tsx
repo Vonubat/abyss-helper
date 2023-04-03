237285e89c8c8a8c0e4e4ac6cd1caeeabe02dd61
Tabunov Egor <Vonubat@gmail.com>
2023-04-04 00:19:28 +0300

    fix: minor bug in order

--- src/components/card/CardsContainer.tsx
+++ src/components/card/CardsContainer.tsx
@@ -6,10 +6,13 @@ const CardsContainer = (): JSX.Element => {
   const { abysses } = useAppSelector(abyssSelector);
 
   return (
-    <div className="scrollbar-thin scrollbar-thumb-blue-700 scrollbar-track-blue-300 flex w-[95vw] flex-nowrap items-start gap-3 overflow-x-auto overflow-y-hidden rounded-md bg-white bg-opacity-20 p-5">
-      {abysses.map((abyss) => (
-        <Card key={abyss.created} abyss={abyss} />
-      ))}
+    <div className="flex w-[95vw] flex-nowrap items-start gap-3 overflow-x-auto overflow-y-hidden rounded-md bg-white bg-opacity-20 p-5 scrollbar-thin scrollbar-track-blue-300 scrollbar-thumb-blue-700">
+      {abysses
+        .slice()
+        .sort(({ created: a }, { created: b }) => b! - a!)
+        .map((abyss) => (
+          <Card key={abyss.created} abyss={abyss} />
+        ))}
     </div>
   );
 };
