6be2fc1c39454d2c62259bdd1c6d5b5165eec041
Tabunov Egor <Vonubat@gmail.com>
2023-04-04 00:51:47 +0300

    fix: major bug with bg image

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # abyss-helper
 
+https://abyss-helper.netlify.app/
 
 ## Description
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,12 @@
+import bgUrl from '../assets/images/bg.jpg';
 import { CardsContainer, Control } from '../components';
 
 const MainPage = (): JSX.Element => {
   return (
-    <main className="main-page__wrapper flex grow flex-col items-center gap-5 bg-[url('./assets/bg.jpg')] bg-cover py-5">
+    <main
+      className="main-page__wrapper flex grow flex-col items-center gap-5 bg-cover py-5"
+      style={{ backgroundImage: `url(${bgUrl})` }}
+    >
       <Control />
       <CardsContainer />
     </main>
