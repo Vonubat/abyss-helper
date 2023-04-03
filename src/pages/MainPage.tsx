997bea39acb5fa0b02c04d9242524a7c5f893401
Tabunov Egor <Vonubat@gmail.com>
2023-04-04 00:34:12 +0300

    fix: bg image

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import { CardsContainer, Control } from '../components';
 
 const MainPage = (): JSX.Element => {
   return (
-    <main className="main-page__wrapper flex grow flex-col items-center gap-5 bg-[url('./assets/bg.webp')] bg-cover py-5">
+    <main className="main-page__wrapper flex grow flex-col items-center gap-5 bg-[url('./assets/bg.jpg')] bg-cover py-5">
       <Control />
       <CardsContainer />
     </main>
